e0261a6e3b515a4f69e75c64028b398bf2a1a5a8
Egor Martynenko <egor.martinenko01@gmail.com>
2024-10-03 18:34:04 +0300

    Create ReactAPP vs MUI for todo-fe-lessons

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
-import './index.css'
+import {CssBaseline} from "@mui/material";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <>
+        <CssBaseline/>
+        <App />
+    </>
+
 )
